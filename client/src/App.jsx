e7050eb7e879ff7399709453b46aed481b81ad63
sachin <sachinpokhriyal27@gmail.com>
2024-12-09 16:31:42 +0530

    install react js and tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-3xl text-red-100'>
+        App is Here
+      </div>
+    </>
+  )
+}
+
+export default App
